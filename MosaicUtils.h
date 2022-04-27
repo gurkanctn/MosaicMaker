82b441c561587dbaab2d795cb8268e53cb792625
Aircraft Guy <8074474+gurkanctn@users.noreply.github.com>
2022-04-27 21:58:17 +0300

    v0.3a
    +color matching algorithm
    + higher resolution limit
    + progress bar

--- MosaicUtils.h
+++ MosaicUtils.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <filesystem>
 
 std::string getFileNameExtension(std::string s) {
 	size_t i = s.rfind('.', s.length());
@@ -11,7 +12,22 @@ std::string getFileNameExtension(std::string s) {
 
 std::string targetFileName;
 
+typedef struct {
+	unsigned char r, g, b;
+} RGB;
+
+double ColourDistance(RGB e1, RGB e2)
+{
+	long rmean = ((long)e1.r + (long)e2.r) / 2;
+	long r = (long)e1.r - (long)e2.r;
+	long g = (long)e1.g - (long)e2.g;
+	long b = (long)e1.b - (long)e2.b;
+	return sqrt((((512 + rmean) * r * r) >> 8) + 4 * g * g + (((767 - rmean) * b * b) >> 8));
+}
+
+
 int nPhotos = 0;
+int nFilesToProcess = 0;
 int nPhotoCounter = 0;
 int nProblems = 0;
 std::vector<std::string> sFileNames;
@@ -22,10 +38,16 @@ int XGridCount = 5;		//will be recalculated depending on Target photo size
 int YGridCount = 5;		//will be recalculated depending on Target photo size
 
 int brightnessLookup[255];
+int LookupRGB[32][32][32];
+
 
 float TargetBrightness[400][400];		//400x400 should be enough for everyone (kidding)
+RGB TargetRGB[400][400];		//400x400 should be enough for everyone (kidding)
 
+float fGlobalTime = 0.0f;
 bool bShowTarget = false;
+bool bTargetAlreadyDisplayed = false;
+bool bTargetMosaicGenerated = false;
 bool bWrittenToFile = false;
 
 void GenerateFileList() {
@@ -51,4 +73,20 @@ void GenerateFileList() {
 	std::cout << nPhotos << " photos found!..." << std::endl;
 	if (nProblems > 0) std::cout << nProblems << " files ignored..." << std::endl;
 
+}
+
+float progress = 0.0f;
+
+void displayProgressBar(float progress) {
+	//display Progress bar
+	int barWidth = 70;
+	std::cout << "[";
+	int pos = barWidth * progress;
+	for (int j = 0; j < barWidth; ++j) {
+		if (j < pos) std::cout << "=";
+		else if (j == pos) std::cout << ">";
+		else std::cout << " ";
+	}
+	std::cout << "] " << int(progress * 100.0) << "%\r";
+	std::cout.flush();
 }
--- main.cpp
+++ main.cpp
@@ -13,13 +13,14 @@
 class MosaicMaker : public olc::PixelGameEngine
 {
 public:
-	MosaicMaker() {		sAppName = "Mosaic Maker v0.2a - Gurkan Cetin, 2022";	}
+	MosaicMaker() {		sAppName = "Mosaic Maker v0.3a - Gurkan Cetin, 2022";	}
 
 public:
 	
 	struct PhotoData {
 		int index;
 		float brightness;
+		RGB rgb;
 		olc::Sprite* thumbnail;
 		bool operator<(const PhotoData& a) const {
 			return brightness < a.brightness;
@@ -58,6 +59,32 @@ public:
 		return brightnessSum / (nFrameHeight * nFrameWidth);
 	}
 
+	RGB calcFrameRGB(olc::Sprite* spr) {
+		int nFrameWidth = spr->width;
+		int nFrameHeight = spr->height;
+		RGB FrameRGB = {0.0, 0.0, 0.0};
+		long tempR = 0;
+		long tempG = 0;
+		long tempB = 0;
+		for (int i = 0; i < nFrameWidth; i++){
+			for (int j = 0; j < nFrameHeight; j++)
+			{
+				olc::Pixel p = spr->GetPixel(j, i);
+				tempR += p.r;
+				tempG += p.g;
+				tempB += p.b;
+			}
+		}
+		tempR /= (nFrameHeight * nFrameWidth);
+		tempG /= (nFrameHeight * nFrameWidth);
+		tempB /= (nFrameHeight * nFrameWidth);
+		FrameRGB.r = (unsigned char)tempR;
+		FrameRGB.g = (unsigned char)tempG;
+		FrameRGB.b = (unsigned char)tempB;
+		return FrameRGB;
+	}
+
+
 	void WriteResultToFile() {
 		olc::Sprite* pScreenPtr = GetDrawTarget();
 		try {
@@ -77,19 +104,20 @@ public:
 				//t.Scale(2, 2);
 				SetDrawTarget(sprTarget);
 				DrawSprite(0, 0, sprTargetOriginal, 2, 0);
+
 				delete sprTargetOriginal;
 		}
 		catch (...) {
-			std::cout << "Couldn't open your file, Correct it or die!" << std::endl;
+			std::cout << "Couldn't open your file! Correct it or die!" << std::endl;
 			return false;
 		}
 
 		XGridCount = sprTarget->width / XGridSize;
 		YGridCount = sprTarget->height / YGridSize;
 		
-		int nFilesToProcess = sFileNames.size();
+		nFilesToProcess = sFileNames.size();
 		std::cout << "Starting to load all photos, this may take a while. Please standby..." << std::endl;
-
+		
 		for (int i = 0; i < nFilesToProcess; i++) {
 			sprTemporary = new olc::Sprite(sFileNames[i]);
 			int scale = sprTemporary->width / XGridSize;
@@ -98,13 +126,19 @@ public:
 			DrawFrame(sprTemporary, 0, 0, scale);
 			sprPhotos.push_back(sprDummy);
 			delete sprTemporary;
+			displayProgressBar(progress);
+			progress += static_cast<float>(1) / nFilesToProcess;
 		}
+		std::cout << std::endl;
 		std::cout << "Finished loading all files. Let's Go!" << std::endl;
-
+		return true;
+	}
+	void GenerateTargetMosaic() {
 		for (int i = 0; i < nFilesToProcess; i++) {
 			PhotoData tempPD;
 			tempPD.index = i;
 			tempPD.brightness = calcFrameBrightness(sprPhotos[i]);
+			tempPD.rgb = calcFrameRGB(sprPhotos[i]);
 			tempPD.thumbnail = sprPhotos[i];
 			PD.push_back(tempPD);
 		}
@@ -124,36 +158,98 @@ public:
 			}
 		}
 
-		//TARGET PHOTO
+		//create Color Lookup Table (3D matrix)
+		// fill up a cube of red,green,blue arrays with best matching photo (j of nFilesToProcess)
+		RGB dummyRGB{0,0,0};
+		for (int i = 0; i < 32; i++) {
+			for (int j = 0; j < 32; j++) {
+				for (int k = 0; k < 32; k++) {
+					float errColor = 1000; //check
+
+					for (int n = 0; n < nFilesToProcess; n++) {
+						dummyRGB.r = i*8;
+						dummyRGB.g = j*8;
+						dummyRGB.b = k*8;
+						double ColDistance = ColourDistance(PD[n].rgb, dummyRGB);
+						if (ColDistance < errColor)
+						{
+							LookupRGB[i][j][k] = n;
+							errColor = ColDistance;
+						}
+					}
+				}
+			}
+		}
+
+
+		//Generate TARGET PHOTO brightness map.
+		if (false) { // for brightness
+			for (int m = 0; m < XGridCount; m++) {
+				for (int n = 0; n < YGridCount; n++) {
+					sprDummy = new olc::Sprite(XGridSize, YGridSize);
+					SetDrawTarget(sprDummy);
+					DrawPartialSprite(0, 0, sprTarget, m * XGridSize, n * YGridSize, XGridSize, YGridSize, 1, 0);
+					TargetBrightness[m][n] = calcFrameBrightness(sprDummy);
+				}
+			}
+		}
+
 		for (int m = 0; m < XGridCount; m++) {
 			for (int n = 0; n < YGridCount; n++) {
 				sprDummy = new olc::Sprite(XGridSize, YGridSize);
 				SetDrawTarget(sprDummy);
 				DrawPartialSprite(0, 0, sprTarget, m * XGridSize, n * YGridSize, XGridSize, YGridSize, 1, 0);
-				TargetBrightness[m][n] = calcFrameBrightness(sprDummy);
+				TargetRGB[m][n] = calcFrameRGB(sprDummy);
 			}
 		}
+		
 
 		std::cout << "Loading Complete!" << std::endl;
 		std::cout << sprPhotos.size() << " photos in Memory!" << std::endl;
 
-		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override {
-		SetDrawTarget(sprTarget);
-		Clear(olc::BLACK);
-		SetPixelMode(olc::Pixel::NORMAL); // Draw all pixels
-		if (GetKey(olc::Key::ESCAPE).bReleased) return false;
+		fGlobalTime += fElapsedTime;
 
+		if (GetKey(olc::Key::ESCAPE).bReleased) return false;
+		
+		if (!bTargetAlreadyDisplayed) {
+			SetDrawTarget(nullptr);
+			SetPixelMode(olc::Pixel::NORMAL); // Draw all pixels
+			DrawSprite(0, 0, sprTarget, 1, 0);
+			bTargetAlreadyDisplayed = true;
+			std::cout << "Target Photo displayed." << std::endl;
+			return true;
+		}
+
+		if ((!bTargetMosaicGenerated) && (fGlobalTime>1.0f)) {
+			GenerateTargetMosaic();
+			bTargetMosaicGenerated = true;
+			std::cout << "Target Mosaic integration complete." << std::endl;
+			return true;
+		}
+			
 		if (!bWrittenToFile) {
+			SetDrawTarget(sprTarget);
+			Clear(olc::BLACK);
+			SetPixelMode(olc::Pixel::NORMAL); // Draw all pixels
 			for (int m = 0; m < XGridCount; m++) {
 				for (int n = 0; n < YGridCount; n++) {
-					DrawSprite(m * XGridSize, n * YGridSize, sprPhotos[brightnessLookup[int(TargetBrightness[m][n])]]);
+					//DrawSprite(m * XGridSize, n * YGridSize, sprPhotos[brightnessLookup[int(TargetBrightness[m][n])]]);
+					unsigned char r = (TargetRGB[m][n].r)/8;
+					unsigned char g = (TargetRGB[m][n].g)/8;
+					unsigned char b = (TargetRGB[m][n].b)/8;
+					int nFile = LookupRGB[r][g][b];
+//					std::cout << "r: " << (int)r <<" g:"<< (int)g <<" b:" << (int)b << " nFile:" << nFile << std::endl;
+					DrawSprite(m * XGridSize, n * YGridSize, sprPhotos[nFile]);
 				}
 			}
 			WriteResultToFile();
 			bWrittenToFile = true;
+			std::cout << "Target Mosaic written to file." << std::endl;
+			std::cout << "Press ESC to quit Mosaic Maker." << std::endl;
+			return true;
 		}
 
 		if (GetKey(olc::Key::TAB).bReleased) bShowTarget = !bShowTarget;
@@ -167,8 +263,8 @@ public:
 
 int main()
 {
-	printf("Mosaic Maker v0.2a. G.Çetin.\n");
-	printf("Will generate a Mosaic photo from 'Photos' folder.\n");
+	std::cout << "Mosaic Maker v0.3a. G.Çetin.\n";
+	std::cout << "Will generate a Mosaic photo from 'Photos' folder.\n";
 	try {
 		GenerateFileList();
 	}
@@ -179,7 +275,7 @@ int main()
 
 	//SET and LOAD TARGET PHOTO
 
-	std::cout << "ENTER TARGET PHOTO FILENAME. Make sure it is under 'photos' folder." << std::endl;
+	std::cout << std::endl << "ENTER TARGET PHOTO FILENAME. Make sure it is under 'photos' folder." << std::endl;
 	std::cin >> targetFileName;
 	targetFileName = "photos/" + targetFileName;
 	std::cout << targetFileName << std::endl;
